cb8d4a6a7beedddbbe469eb49d0c435efa60463d
InMemoriam7th <62008705+InMemoriam7th@users.noreply.github.com>
2021-05-05 15:12:57 +0200

    partie fonctionnel

--- src/client.inc.php
+++ src/client.inc.php
@@ -54,27 +54,73 @@
             return $this->email;
         }
 
-        private function verifyemail(){
+        private function verifyEmail(){
             if(!(filter_var($this->getEmail(), FILTER_VALIDATE_EMAIL))){
-                return "<p>erreur mail</p>";
+                return "<p>Le mail indiqué est impossible</p>";
+            }
+
+        }
+
+        private function verifyAge(){
+            if(!is_numeric($this->getAge())){
+                return "<p>L'âge indiqué est impossible</p>";
             }
 
         }
 
         private function verifyvalueempty(){
-            if(empty($this->getNom()) && empty($this->email)){
-                return "<p>Des champs sont vide</p>";
+            if(empty($this->getNom()) && empty($this->getPrenom()) && empty($this->getAge()) && empty($this->getEmail())){
+                return "<p>Des champs sont vides</p>";
             }
 
         }
 
-        public function add_ddb(){
-            $erreur = $this->verifyemail();
+        private function verify(){
+            $erreur = $this->verifyAge();
             if(isset($erreur)){
                 return $erreur;
             }
-            
+            $erreur = $this->verifyEmail();
+            if(isset($erreur)){
+                return $erreur;
+            }
+            $erreur = $this->verifyvalueempty();
+            if(isset($erreur)){
+                return $erreur;
+            }
+        }
 
+        public function add_ddb(){
+            $erreur = $this->verify;
+            if(isset($erreur)){
+                return $erreur;
+            }
+            $ddb = new ddb();
+            $ddb->insert("INSERT INTO clients (nom, prenom, age, email) VALUES (?,?,?,?)", array($this->getNom(), $this->getPrenom(), $this->getAge() ,$this->getEmail()));
+            header("location:index.php");
+            exit();
+        }
+
+        public function update_ddb(){
+            $erreur = $this->verify;
+            if(isset($erreur)){
+                return $erreur;
+            }
+            $ddb = new ddb();
+            $ddb->update("UPDATE clients SET nom = ?, prenom = ?, age = ? , email = ? WHERE id_client = ?", array($this->getNom(), $this->getPrenom(), $this->getAge() ,$this->getEmail(), $this->getIdClient()));
+            header("location:index.php");
+            exit();
+        }
+
+        public function delete_ddb(){
+            $erreur = $this->verify;
+            if(isset($erreur)){
+                return $erreur;
+            }
+            $ddb = new ddb();
+            $ddb->delete("DELETE FROM clients WHERE id_client = ? ", array($this->getIdClient()));
+            header("location:index.php");
+            exit();
         }
 
     }
--- src/ddb.inc.php
+++ src/ddb.inc.php
@@ -6,38 +6,61 @@
         const password = "";
     
 
-    private function connection(){
-        try{
-            $bdd_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            $bdd = new PDO('mysql:host='.self::host.';dbname='.self::dbname.'', self::user, self::password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',$bdd_options));
-            return $bdd;
-        }catch(Exception $e){
-            print '<p>Impossible de se connecter a la base de donnée</p>';
+        private function connection(){
+            try{
+                $bdd_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+                $bdd = new PDO('mysql:host='.self::host.';dbname='.self::dbname.'', self::user, self::password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',$bdd_options));
+                return $bdd;
+            }catch(Exception $e){
+                print '<p>Impossible de se connecter a la base de donnée</p>';
+            }
+            
+        }
+    
+    
+        public function insert($sql_requete, $arg_sql){
+            $bdd = $this->connection();
+            if(isset($bdd)){
+                $req = $bdd->prepare($sql_requete);
+                $req->execute($arg_sql);
+                $req->closeCursor();
+                return true;
+            }
+            return false;
+        }
+    
+        public function select($sql_requete){
+            $bdd = $this->connection();
+            if(isset($bdd)){
+                $req = $bdd->prepare($sql_requete);
+                $req->execute();
+                $result = $req->fetchAll();
+                $req->closeCursor();
+                return $result;
+            }
+        }
+    
+        public function update($sql_requete, $arg_sql){
+            $bdd = $this->connection();
+            if(isset($bdd)){
+                $req = $bdd->prepare($sql_requete);
+                $req->execute($arg_sql);
+                $req->closeCursor();
+                return true;
+            }
+            return false;
+        }
+    
+        public function delete($sql_requete, $arg_sql){
+            $bdd = $this->connection();
+            if(isset($bdd)){
+                $req = $bdd->prepare($sql_requete);
+                $req->execute($arg_sql);
+                $req->closeCursor();
+                return true;
+            }
+            return false;
         }
         
     }
-
-
-    public function insert($sql_requete, $arg_sql){
-        $bdd = $this->connection();
-        if(isset($bdd)){
-            $req = $bdd->prepare($sql_requete);
-            $req->execute($arg_sql);
-            $req->closeCursor();
-            return true;
-        }
-        return false;
-    }
-
-    public function select($sql_requete){
-        $bdd = $this->connection();
-        if(isset($bdd)){
-            $req = $bdd->prepare($sql_requete);
-            $req->execute();
-            $result = $req->fetchAll();
-            $req->closeCursor();
-            return $result;
-        }
-    }
-}
 ?>
